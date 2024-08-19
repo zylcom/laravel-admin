418bf2e6c7ffecef24b2f5828151aec99ba3e9d5
sabilillah <sabilillah272@gmail.com>
2024-08-19 18:40:45 +0700

    Update welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { Head, Link } from '@inertiajs/vue3';
-import NavLink from '../Components/NavLink.vue'
+import { Head, Link, usePage } from "@inertiajs/vue3";
+import NavLink from "../Components/NavLink.vue";
 
 defineProps<{
     canLogin?: boolean;
@@ -9,32 +9,32 @@ defineProps<{
     phpVersion: string;
 }>();
 
+const user = usePage().props.auth.user;
 </script>
 
 <template>
-
     <Head title="Welcome" />
 
     <div>
-        <header class='px-9 py-4'>
-            <nav class="w-full flex justify-between">
+        <header class="px-9 py-4">
+            <nav class="flex w-full justify-between">
                 <Link href="/">Invtry</Link>
 
-                <ul class="flex gap-4">
+                <ul class="flex gap-4" v-if="!user">
                     <li>
-                        <NavLink href='/login'>Login</NavLink>
+                        <NavLink href="/login">Login</NavLink>
                     </li>
                     <li>
-                        <NavLink href='/register'>Register</NavLink>
+                        <NavLink href="/register">Register</NavLink>
                     </li>
                 </ul>
+
+                <NavLink href="/dashboard" v-else>Dashboard</NavLink>
             </nav>
         </header>
 
         <div class="px-9">
-            <h1 class="text-5xl">
-                Welcome World!
-            </h1>
+            <h1 class="text-5xl">Welcome World!</h1>
         </div>
     </div>
 </template>
